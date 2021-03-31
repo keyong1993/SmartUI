192390b72c53a45b37eac9b47dc15acbcabaf57b
柯勇 <1678167865@qq.com>
2021-03-31 17:40:04 +0800

    Slider新增Tooltip 修改ToolTip样式

---
+++ SmartUI/Assist/PopupAssist.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls.Primitives;
+
+namespace SmartUI.Assist
+{
+    public class PopupAssist
+    {
+        public static bool GetIsFollowTarget(DependencyObject obj)
+        {
+            return (bool)obj.GetValue(IsFollowTargetProperty);
+        }
+
+        public static void SetIsFollowTarget(DependencyObject obj, bool value)
+        {
+            obj.SetValue(IsFollowTargetProperty, value);
+        }
+
+        public static readonly DependencyProperty IsFollowTargetProperty =
+            DependencyProperty.RegisterAttached("IsFollowTarget", typeof(bool), typeof(PopupAssist), new PropertyMetadata(false, IsFollowTargetChanged));
+
+        private static void IsFollowTargetChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is Popup popup && e.NewValue is bool flag)
+            {
+                if (flag)
+                {
+                    //popup.PlacementTarget.
+                }
+            }
+        }
+    }
+}
---
+++ SmartUI/Converter/NumberConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace SmartUI.Converter
+{
+    public class NumberConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string format = "F2";
+            if (parameter != null)
+                format = parameter.ToString();
+            if (value != null && decimal.TryParse(value.ToString(), out decimal num))
+                return num.ToString(format);
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value;
+        }
+    }
+}
